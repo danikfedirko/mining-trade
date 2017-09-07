3e089361348ce287275b367076e49d077d550731
andriymv <andriymukhametov@gmail.com>
2017-09-07 18:46:22 +0300

    change svg

--- js/main.js
+++ js/main.js
@@ -3,6 +3,74 @@ var width = $( window ).width();
 if(width<770){
  $('#grafic').attr('src','img/grafic-mobile.svg')   
 }
-
-
+$( ".cd-block-1 .cd-timeline-content" )
+    .mouseenter(function() {
+        $(".cd-block-1 .cd-timeline-img img").attr('src','img/1-color.svg')
+        $(".cd-block-2 .cd-timeline-img img").attr('src','img/num2.svg')
+        $(".cd-block-3 .cd-timeline-img img").attr('src','img/num3.svg')
+        $(".cd-block-4 .cd-timeline-img img").attr('src','img/num4.svg')
+        $(".cd-block-5 .cd-timeline-img img").attr('src','img/num5.svg')
+        $(".cd-block-6 .cd-timeline-img img").attr('src','img/num6.svg')
+        $(".cd-block-7 .cd-timeline-img img").attr('src','img/num7.svg')
+    });
+$( ".cd-block-2 .cd-timeline-content" )
+    .mouseenter(function() {
+        $(".cd-block-2 .cd-timeline-img img").attr('src','img/2-color.svg')
+        $(".cd-block-1 .cd-timeline-img img").attr('src','img/num1.svg')
+        $(".cd-block-3 .cd-timeline-img img").attr('src','img/num3.svg')
+        $(".cd-block-4 .cd-timeline-img img").attr('src','img/num4.svg')
+        $(".cd-block-5 .cd-timeline-img img").attr('src','img/num5.svg')
+        $(".cd-block-6 .cd-timeline-img img").attr('src','img/num6.svg')
+        $(".cd-block-7 .cd-timeline-img img").attr('src','img/num7.svg')
+    });
+$( ".cd-block-3 .cd-timeline-content" )
+    .mouseenter(function() {
+        $(".cd-block-3 .cd-timeline-img img").attr('src','img/3-color.svg')
+        $(".cd-block-1 .cd-timeline-img img").attr('src','img/num1.svg')
+        $(".cd-block-2 .cd-timeline-img img").attr('src','img/num2.svg')
+        $(".cd-block-4 .cd-timeline-img img").attr('src','img/num4.svg')
+        $(".cd-block-5 .cd-timeline-img img").attr('src','img/num5.svg')
+        $(".cd-block-6 .cd-timeline-img img").attr('src','img/num6.svg')
+        $(".cd-block-7 .cd-timeline-img img").attr('src','img/num7.svg')
+    });
+$( ".cd-block-4 .cd-timeline-content" )
+    .mouseenter(function() {
+        $(".cd-block-4 .cd-timeline-img img").attr('src','img/4-color.svg')
+        $(".cd-block-1 .cd-timeline-img img").attr('src','img/num1.svg')
+        $(".cd-block-2 .cd-timeline-img img").attr('src','img/num2.svg')
+        $(".cd-block-3 .cd-timeline-img img").attr('src','img/num3.svg')
+        $(".cd-block-5 .cd-timeline-img img").attr('src','img/num5.svg')
+        $(".cd-block-6 .cd-timeline-img img").attr('src','img/num6.svg')
+        $(".cd-block-7 .cd-timeline-img img").attr('src','img/num7.svg')
+    });
+$( ".cd-block-5 .cd-timeline-content" )
+    .mouseenter(function() {
+        $(".cd-block-5 .cd-timeline-img img").attr('src','img/5-color.svg')
+        $(".cd-block-1 .cd-timeline-img img").attr('src','img/num1.svg')
+        $(".cd-block-2 .cd-timeline-img img").attr('src','img/num2.svg')
+        $(".cd-block-3 .cd-timeline-img img").attr('src','img/num3.svg')
+        $(".cd-block-4 .cd-timeline-img img").attr('src','img/num4.svg')
+        $(".cd-block-6 .cd-timeline-img img").attr('src','img/num6.svg')
+        $(".cd-block-7 .cd-timeline-img img").attr('src','img/num7.svg')
+    });
+$( ".cd-block-6 .cd-timeline-content" )
+    .mouseenter(function() {
+        $(".cd-block-6 .cd-timeline-img img").attr('src','img/6-color.svg')
+        $(".cd-block-1 .cd-timeline-img img").attr('src','img/num1.svg')
+        $(".cd-block-2 .cd-timeline-img img").attr('src','img/num2.svg')
+        $(".cd-block-3 .cd-timeline-img img").attr('src','img/num3.svg')
+        $(".cd-block-4 .cd-timeline-img img").attr('src','img/num4.svg')
+        $(".cd-block-5 .cd-timeline-img img").attr('src','img/num5.svg')
+        $(".cd-block-7 .cd-timeline-img img").attr('src','img/num7.svg')
+    });
+$( ".cd-block-7 .cd-timeline-content" )
+    .mouseenter(function() {
+        $(".cd-block-7 .cd-timeline-img img").attr('src','img/7-color.svg')
+        $(".cd-block-1 .cd-timeline-img img").attr('src','img/num1.svg')
+        $(".cd-block-2 .cd-timeline-img img").attr('src','img/num2.svg')
+        $(".cd-block-3 .cd-timeline-img img").attr('src','img/num3.svg')
+        $(".cd-block-4 .cd-timeline-img img").attr('src','img/num4.svg')
+        $(".cd-block-5 .cd-timeline-img img").attr('src','img/num5.svg')
+        $(".cd-block-6 .cd-timeline-img img").attr('src','img/num6.svg')
+    });
 
